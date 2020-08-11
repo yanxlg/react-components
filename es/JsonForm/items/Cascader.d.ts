f4cf8a2eff88392029cca69ac31842ebfafbfc86
bbhuang <bbhuang@vova.com.hk>
2020-08-11 16:29:52 +0800

    update version

--- es/JsonForm/items/Cascader.d.ts
+++ es/JsonForm/items/Cascader.d.ts
@@ -4,10 +4,13 @@ import { FormInstance, Rule } from 'antd/es/form';
 import { FormItemLabelProps } from 'antd/es/form/FormItemLabel';
 import { CascaderProps as AntdCascaderProps } from 'antd/es/cascader/index';
 import { FormatterType } from '../../utils/formatter';
+import { IOptionItem } from './Select';
 export declare type CascaderType = 'cascader';
+declare type OptionsPromise = () => Promise<IOptionItem[]>;
 export declare type CascaderProps<T = string> = FormItemLabelProps & CustomFormProps & {
     type: CascaderType;
     form: FormInstance;
+    optionList?: IOptionItem[] | OptionsPromise;
     placeholder?: string;
     className?: string;
     formItemClassName?: string;
