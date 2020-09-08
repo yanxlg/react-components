6c1784e13c7a58ef216c5afc5fb022d337a5a371
xlyan <xlyan@vova.com.hk>
2020-09-08 16:22:01 +0800

    update

--- es/JsonForm/index.d.ts
+++ es/JsonForm/index.d.ts
@@ -37,36 +37,7 @@ import { LabelProps as LabelPropsV2 } from './items/v2/Label';
 export declare interface CustomFormProps {
     labelClassName?: string;
 }
-export declare type FormField<T = string> = (
-    | Omit<InputProps<T>, 'form'>
-    | Omit<LabelProps<T>, 'form'>
-    | Omit<FormPasswordProps<T>, 'form'>
-    | Omit<SelectProps<T>, 'form'>
-    | Omit<CheckboxProps<T>, 'form'>
-    | Omit<DatePickerProps<T>, 'form'>
-    | Omit<DateRangerProps<T>, 'form'>
-    | Omit<CheckboxGroupProps<T>, 'form'>
-    | Omit<RadioGroupProps<T>, 'form'>
-    | Omit<InputRangeProps<T>, 'form'>
-    | Omit<TextAreaProps<T>, 'form'>
-    | Omit<LayoutProps<T>, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'>
-    | Omit<CollapseLayoutProps<T>, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'>
-    | Omit<DynamicItemProps, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'>
-    | Omit<HideItemProps, 'form'>
-    | Omit<CascaderProps, 'form'>
-    | Omit<CustomFragmentProps, 'form'>
-    | Omit<TreeSelectProps<T>, 'form'>
-    | Omit<LoadingItemProps<T>, 'form'>
-    | Omit<NumberRangeProps<T>, 'form'>
-    | Omit<FormTreeProps<T>, 'form'>
-    | Omit<SelectPropsV2, 'form'>
-    | Omit<InputPropsV2, 'form'>
-    | Omit<CheckboxGroupPropsV2, 'form'>
-    | Omit<DateRangerPropsV2, 'form'>
-    | Omit<NumberRangePropsV2, 'form'>
-    | Omit<RadioGroupPropsV2, 'form'>
-    | Omit<LabelPropsV2<T>, 'form'>
-) & {
+export declare type FormField<T = string> = (Omit<InputProps<T>, 'form'> | Omit<LabelProps<T>, 'form'> | Omit<FormPasswordProps<T>, 'form'> | Omit<SelectProps<T>, 'form'> | Omit<CheckboxProps<T>, 'form'> | Omit<DatePickerProps<T>, 'form'> | Omit<DateRangerProps<T>, 'form'> | Omit<CheckboxGroupProps<T>, 'form'> | Omit<RadioGroupProps<T>, 'form'> | Omit<InputRangeProps<T>, 'form'> | Omit<TextAreaProps<T>, 'form'> | Omit<LayoutProps<T>, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'> | Omit<CollapseLayoutProps<T>, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'> | Omit<DynamicItemProps, 'form' | 'labelClassName' | 'itemCol' | 'itemRow'> | Omit<HideItemProps, 'form'> | Omit<CascaderProps, 'form'> | Omit<CustomFragmentProps, 'form'> | Omit<TreeSelectProps<T>, 'form'> | Omit<LoadingItemProps<T>, 'form'> | Omit<NumberRangeProps<T>, 'form'> | Omit<FormTreeProps<T>, 'form'> | Omit<SelectPropsV2, 'form'> | Omit<InputPropsV2, 'form'> | Omit<CheckboxGroupPropsV2, 'form'> | Omit<DateRangerPropsV2, 'form'> | Omit<NumberRangePropsV2, 'form'> | Omit<RadioGroupPropsV2, 'form'> | Omit<LabelPropsV2<T>, 'form'>) & {
     form?: FormInstance;
     key?: string;
 };
@@ -89,38 +60,8 @@ export interface JsonFormRef {
     resetFields: (fields?: NamePath[]) => void;
     getOriginValues: () => Store;
 }
-export declare const getColChildren: (
-    children: React.ReactElement<
-        any,
-        | string
-        | ((
-              props: any,
-          ) => React.ReactElement<
-              any,
-              string | any | (new (props: any) => React.Component<any, any, any>)
-          >)
-        | (new (props: any) => React.Component<any, any, any>)
-    >,
-    itemCol?: ColProps,
-    times?: number,
-) => JSX.Element;
-export declare const getFormItem: (
-    { type, ...field }: FormField<string>,
-    form: FormInstance,
-    labelClassName?: string,
-    itemCol?: ColProps,
-    itemRow?: RowProps,
-    index?: number,
-    hide?: boolean,
-) => JSX.Element;
-export declare const getFormItems: (
-    fieldList: FormField<string>[],
-    form: FormInstance,
-    labelClassName?: string,
-    itemCol?: ColProps,
-    itemRow?: RowProps,
-    showList?: string[],
-) => JSX.Element | JSX.Element[];
-declare const _default_1: React.ForwardRefExoticComponent<JsonFormProps<any> &
-    React.RefAttributes<JsonFormRef>>;
+export declare const getColChildren: (children: React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, itemCol?: ColProps, times?: number) => JSX.Element;
+export declare const getFormItem: ({ type, ...field }: FormField<string>, form: FormInstance, labelClassName?: string, itemCol?: ColProps, itemRow?: RowProps, index?: number, hide?: boolean) => JSX.Element;
+export declare const getFormItems: (fieldList: FormField<string>[], form: FormInstance, labelClassName?: string, itemCol?: ColProps, itemRow?: RowProps, showList?: string[]) => JSX.Element | JSX.Element[];
+declare const _default_1: React.ForwardRefExoticComponent<JsonFormProps<any> & React.RefAttributes<JsonFormRef>>;
 export default _default_1;
