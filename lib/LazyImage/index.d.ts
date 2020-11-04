355edc614e4ce8f6262754e1bf0ae82e346d9e95
xlyan <xlyan@vova.com.hk>
2020-11-04 10:08:14 +0800

    add yarnrc

--- lib/LazyImage/index.d.ts
+++ lib/LazyImage/index.d.ts
@@ -1,5 +1,21 @@
 import React from 'react';
 import { LazyLoadProps } from 'react-lazyload';
-export declare type LazyImageProps = React.ImgHTMLAttributes<HTMLImageElement> & Omit<LazyLoadProps, "children" | "placeholder">;
-declare const _default: React.ForwardRefExoticComponent<React.ImgHTMLAttributes<HTMLImageElement> & Pick<LazyLoadProps, "height" | "once" | "offset" | "overflow" | "resize" | "scroll" | "throttle" | "debounce" | "scrollContainer" | "unmountIfInvisible" | "preventLoading"> & React.RefAttributes<unknown>>;
+export declare type LazyImageProps = React.ImgHTMLAttributes<HTMLImageElement> &
+    Omit<LazyLoadProps, 'children' | 'placeholder'>;
+declare const _default: React.ForwardRefExoticComponent<React.ImgHTMLAttributes<HTMLImageElement> &
+    Pick<
+        LazyLoadProps,
+        | 'height'
+        | 'once'
+        | 'offset'
+        | 'overflow'
+        | 'resize'
+        | 'scroll'
+        | 'throttle'
+        | 'debounce'
+        | 'scrollContainer'
+        | 'unmountIfInvisible'
+        | 'preventLoading'
+    > &
+    React.RefAttributes<unknown>>;
 export default _default;
